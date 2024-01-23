9cc84f075b873f7a0b1eee86c4097a612036ecde
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-01-23 07:58:31 +0530

    Q8 done

---
+++ Q8.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Training_Day_9_
+{
+	public class Q8
+	{
+		public int sumEachDigit(int num)
+		{
+			int sum = 0;
+			while (num>0)
+			{
+				int rem = num % 10;
+				sum += rem;
+				num /= 10;
+			}
+			return sum;
+		}
+	}
+	public class Program
+	{
+		public static void Main()
+		{
+			Q8 obj = new Q8();
+			int num = 1234567898;
+			int result = obj.sumEachDigit(num);
+
+			Console.WriteLine($"The sum of each digit of {num} is {result}");
+
+			Console.ReadLine();
+
+		}
+	}
+}
+
